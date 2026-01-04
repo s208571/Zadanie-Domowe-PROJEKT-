b2b7891b4ca1253226a998932d13e439b1a009aa
s208571 <s208571@student.pg.edu.pl>
2026-01-04 18:52:17 +0100

    Dodanie gry warcaby, finalizacja calego projektu, dodanie opisow, naprawa funkcji

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,4 @@
+//Dawid Tomaszewski s208571 grupa 4a
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -7,7 +8,7 @@
 #include <vector>
 using namespace std;
 
-
+// Funkcja tworzy pusty plik SVG o wymiarach 800x600
 void GenerowaniePustegoSVG()
 {
     cout << "Generuje podstawowy(pusty) plik SVG" << endl;
@@ -18,7 +19,7 @@ void GenerowaniePustegoSVG()
     plik << "</svg>";
     plik.close();
 }
-
+// Funkcja rysuje kształty na podstawie danych wejściowych użytkownika
 void RysowanieKrztaltow()
 {
     int kolo_x, kolo_y, kolo_r;
@@ -102,6 +103,7 @@ bool SprwadzWygranaKK(char planszaKK[3][3], char gracz)
 
     return false;
 }
+// Funkcja wyswietla plansze Kolko i Krzyzyk w konsoli
 void RysowanieKK(char planszaKK[3][3])
 {
     for(int z=0; z<3; z++)
@@ -156,6 +158,7 @@ void AktualizujSVG(char plansza[3][3])
     plik << "</svg>";
     plik.close(); 
 }
+// Algorytm sztucznej inteligencji dla gracza komputerowego
 void RuchKomputera(char gra[3][3], char znakPC, char znakGracza)
 {
     cout << "Komputer mysli..." << endl;
@@ -244,6 +247,229 @@ void RuchKomputera(char gra[3][3], char znakPC, char znakGracza)
         gra[dostepne[los].first][dostepne[los].second] = znakPC;
     }
 }
+const int W_ROZMIAR = 8;
+// Funkcja ustawia poczatkowy stan planszy do Warcabow
+void InicjalizacjaWarcaby(char plansza[W_ROZMIAR][W_ROZMIAR])
+{
+    for (int i = 0; i < W_ROZMIAR; i++)
+    {
+        for (int j = 0; j < W_ROZMIAR; j++)
+        {
+            // Ustawiamy puste pola
+            // ' ' to jasne pole (niegrywalne), '.' to ciemne pole (grywalne, puste)
+            if ((i + j) % 2 == 0) 
+                plansza[i][j] = ' '; 
+            else 
+                plansza[i][j] = '.'; // Kropka ulatwia celowanie
+        }
+    }
+
+    // Ustawienie pionków (tylko na ciemnych polach)
+    // Czarne ('c') na górze (wiersze 0, 1, 2)
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < W_ROZMIAR; j++) {
+            if ((i + j) % 2 != 0) plansza[i][j] = 'c';
+        }
+    }
+
+    // Białe ('b') na dole (wiersze 5, 6, 7)
+    for (int i = 5; i < 8; i++) {
+        for (int j = 0; j < W_ROZMIAR; j++) {
+            if ((i + j) % 2 != 0) plansza[i][j] = 'b';
+        }
+    }
+}
+// Funkcja wyswietla plansze do Warcabow w konsoli
+void RysowanieWarcaby(char plansza[W_ROZMIAR][W_ROZMIAR])
+{
+    cout << "  0 1 2 3 4 5 6 7" << endl; // Nagłówki kolumn
+    cout << " +-----------------+" << endl;
+    for (int i = 0; i < W_ROZMIAR; i++)
+    {
+        cout << i << "|"; // Numer wiersza
+        for (int j = 0; j < W_ROZMIAR; j++)
+        {
+            cout << plansza[i][j] << " ";
+        }
+        cout << "|" << i << endl;
+    }
+    cout << " +-----------------+" << endl;
+}
+// Logika ruchu w Warcabach (sprawdzanie poprawnosci i bicia)
+// Funkcja zwraca true, jeśli ruch się udał, false jeśli był niedozwolony
+bool WykonajRuchWarcaby(char plansza[W_ROZMIAR][W_ROZMIAR], int w1, int k1, int w2, int k2, char gracz)
+{
+    // 1. Sprawdzenie zakresu
+    if (w1 < 0 || w1 > 7 || k1 < 0 || k1 > 7 || w2 < 0 || w2 > 7 || k2 < 0 || k2 > 7) {
+        cout << "Blad: Wyjscie poza plansze!" << endl;
+        return false;
+    }
+
+    // 2. Sprawdzenie wlasciciela (uzywamy tolower, zeby dzialalo dla b i B)
+    char p = plansza[w1][k1];
+    if (tolower(p) != gracz) { 
+        cout << "Blad: To nie twoj pionek!" << endl;
+        return false;
+    }
+
+    // 3. Cel musi byc pusty
+    if (plansza[w2][k2] != '.') {
+        cout << "Blad: Pole zajete!" << endl;
+        return false;
+    }
+
+    int deltaW = w2 - w1;
+    int deltaK = k2 - k1;
+    bool czyDamka = isupper(p); // Czy to duza litera (B lub C)?
+
+    // 4. Kierunek ruchu (tylko jesli NIE jest damka)
+    if (!czyDamka) 
+    {
+        if (gracz == 'b' && deltaW > 0) {
+            cout << "Blad: Zwykly bialy pionek idzie tylko w gore!" << endl;
+            return false;
+        }
+        if (gracz == 'c' && deltaW < 0) {
+            cout << "Blad: Zwykly czarny pionek idzie tylko w dol!" << endl;
+            return false;
+        }
+    }
+
+    // 5. Ruch zwykly (o 1 pole)
+    if (abs(deltaW) == 1 && abs(deltaK) == 1)
+    {
+        plansza[w2][k2] = p;     // Przenosimy pionek (zachowujac czy to b czy B)
+        plansza[w1][k1] = '.';
+        return true;
+    }
+
+    // 6. Bicie (o 2 pola)
+    else if (abs(deltaW) == 2 && abs(deltaK) == 2)
+    {
+        int srodekW = (w1 + w2) / 2;
+        int srodekK = (k1 + k2) / 2;
+        char zbijany = plansza[srodekW][srodekK];
+
+        if (zbijany == '.' || tolower(zbijany) == gracz) {
+            cout << "Blad: Nie mozna bic pustego lub swojego!" << endl;
+            return false;
+        }
+
+        plansza[w2][k2] = p;     // Przenosimy
+        plansza[w1][k1] = '.';   // Czyscimy start
+        plansza[srodekW][srodekK] = '.'; // Usuwamy zbitego
+        return true;
+    }
+
+    return false;
+}
+// Sprawdza czy ktorys gracz stracil wszystkie pionki
+bool CzyKoniecWarcabow(char plansza[W_ROZMIAR][W_ROZMIAR], char &wygrany)
+{
+    int biale = 0;
+    int czarne = 0;
+
+    for(int i=0; i<8; i++) {
+        for(int j=0; j<8; j++) {
+            if(tolower(plansza[i][j]) == 'b') biale++;
+            if(tolower(plansza[i][j]) == 'c') czarne++;
+        }
+    }
+
+    if(biale == 0) { wygrany = 'c'; return true; }
+    if(czarne == 0) { wygrany = 'b'; return true; }
+    return false;
+}
+// Generuje plik SVG dla gry w Warcaby
+void AktualizujSVG_Warcaby(char plansza[8][8])
+{
+    ofstream plik("gra_warcaby.svg");
+    plik << "<?xml version=\"1.0\" standalone=\"no\"?>" << endl;
+    // Plansza 400x400 (każde pole 50x50)
+    plik << "<svg width=\"400\" height=\"400\" xmlns=\"http://www.w3.org/2000/svg\">" << endl;
+
+    // Rysowanie szachownicy
+    for(int i=0; i<8; i++) {
+        for(int j=0; j<8; j++) {
+            int x = j * 50;
+            int y = i * 50;
+            string kolor = ((i+j)%2 == 0) ? "wheat" : "saddlebrown"; // Jasne i ciemne pola
+            
+            plik << "<rect x=\"" << x << "\" y=\"" << y 
+                 << "\" width=\"50\" height=\"50\" fill=\"" << kolor << "\" />" << endl;
+
+            // Rysowanie pionków
+            char p = plansza[i][j];
+            if(p != ' ' && p != '.') // Jeśli jest jakiś pionek
+            {
+                string kolorPionka = (tolower(p) == 'b') ? "white" : "black";
+                string obrys = (tolower(p) == 'b') ? "black" : "white"; // Kontrast
+                
+                // Rysujemy koło (pionek)
+                plik << "<circle cx=\"" << x+25 << "\" cy=\"" << y+25 
+                     << "\" r=\"20\" fill=\"" << kolorPionka 
+                     << "\" stroke=\"" << obrys << "\" stroke-width=\"2\" />" << endl;
+                
+                // Jeśli to damka (Duża litera), dodajmy oznaczenie (np. kropka w srodku)
+                if(isupper(p)) {
+                    plik << "<circle cx=\"" << x+25 << "\" cy=\"" << y+25 
+                         << "\" r=\"5\" fill=\"red\" />" << endl;
+                }
+            }
+        }
+    }
+    plik << "</svg>";
+    plik.close();
+}
+// Zapisuje stan gry (plansze i ture) do pliku tekstowego
+void ZapiszGre(char plansza[8][8], char gracz)
+{
+    ofstream plik("zapis_warcaby.txt");
+    if(plik.is_open())
+    {
+        plik << gracz << endl; // Zapisujemy czyja tura
+        for(int i=0; i<8; i++)
+        {
+            for(int j=0; j<8; j++)
+            {
+                plik << plansza[i][j]; // Zapisujemy plansze znak po znaku
+            }
+            plik << endl;
+        }
+        plik.close();
+        cout << "Gra zostala zapisana!" << endl;
+    }
+    else cout << "Blad zapisu pliku!" << endl;
+}
+// Wczytuje stan gry z pliku tekstowego
+bool WczytajGre(char plansza[8][8], char &gracz)
+{
+    ifstream plik("zapis_warcaby.txt");
+    if(plik.is_open())
+    {
+        plik >> gracz; // Wczytuje gracza (b/c)
+        
+        // Musimy "połknąć" znak nowej linii (enter), który został po wczytaniu gracza
+        char smiec;
+        plik.get(smiec); 
+
+        for(int i=0; i<8; i++)
+        {
+            for(int j=0; j<8; j++)
+            {
+                // Używamy get(), żeby wczytać też spacje!
+                plik.get(plansza[i][j]); 
+            }
+            // Po każdym wierszu w pliku jest enter, musimy go pominąć
+            plik.get(smiec); 
+        }
+        plik.close();
+        cout << "Gra wczytana!" << endl;
+        return true;
+    }
+    cout << "Brak pliku zapisu!" << endl;
+    return false;
+}
 int main() 
 {
     srand(time(NULL)); // Zmienna do losowania
@@ -275,7 +501,7 @@ int main()
         {
             case 1: GenerowaniePustegoSVG(); break;
             case 2: RysowanieKrztaltow(); break;
-            case 3:
+            case 3:{
     liczba_ruchowKK = 0;
     koniec_gryKK = false;
     aktualny_gracz = 'O';
@@ -360,9 +586,9 @@ int main()
                 }
             }
         }
-    }
+    }}
     break;
-            case 4: cout << "Kolko i krzyzyk z komputerem" << endl;
+            case 4: {cout << "Kolko i krzyzyk z komputerem" << endl;
             liczba_ruchowKK = 0;
                 koniec_gryKK = false;
                 // Inicjalizacja planszy
@@ -436,10 +662,92 @@ int main()
                 }
                 if(liczba_ruchowKK == 9 && !koniec_gryKK) {
                      cout << "REMIS!" << endl; cin.get();
-                }
+                }}
                 break;
-            case 5: cout << "Warcaby" << endl; break;
-            case 6: cout << "Pomoc w budowie." << endl; break;
+            case 5: 
+        {
+            char planszaW[8][8];
+            InicjalizacjaWarcaby(planszaW);
+            AktualizujSVG_Warcaby(planszaW);
+
+            char obecny_warcaby = 'b'; 
+            bool koniecW = false;
+            char zwyciezca = ' ';
+
+            while (!koniecW)
+            { 
+                cout << "\n=== WARCABY ===" << endl;
+                cout << "Opcje: 9 9 9 9 (wyjscie), 8 8 8 8 (zapisz), 7 7 7 7 (wczytaj)" << endl;
+                RysowanieWarcaby(planszaW);
+                
+                if(CzyKoniecWarcabow(planszaW, zwyciezca)) {
+                    cout << "KONIEC GRY! Wygrywa: " << (zwyciezca == 'b' ? "BIAŁE" : "CZARNE") << endl;
+                    cin.ignore(); cin.get();
+                    break;
+                }
+
+                cout << "Ruch gracza: " << (obecny_warcaby == 'b' ? "BIALE (b)" : "CZARNE (c)") << endl;
+                cout << "Podaj ruch (w1 k1 w2 k2): ";
+                
+                int w1, k1, w2, k2;
+                if (!(cin >> w1 >> k1 >> w2 >> k2)) { // Zabezpieczenie przed wpisaniem liter
+                    cout << "Blad danych!" << endl;
+                    cin.clear(); cin.ignore(1000, '\n');
+                    continue;
+                }
+
+                // --- OBSLUGA KOMEND SPECJALNYCH ---
+                // Wyjście tylko przy 9 9 9 9
+                if (w1 == 9 && k1 == 9 && w2 == 9 && k2 == 9) { 
+                    koniecW = true; 
+                    break; 
+                }
+                
+                // Zapis tylko przy 8 8 8 8
+                if (w1 == 8 && k1 == 8 && w2 == 8 && k2 == 8) { 
+                    ZapiszGre(planszaW, obecny_warcaby); 
+                    continue; 
+                }
+                
+                // Wczytanie tylko przy 7 7 7 7
+                if (w1 == 7 && k1 == 7 && w2 == 7 && k2 == 7) { 
+                    if(WczytajGre(planszaW, obecny_warcaby)) {
+                        AktualizujSVG_Warcaby(planszaW); // Odswiez SVG po wczytaniu
+                    }
+                    continue; 
+                }
+
+                // --- LOGIKA RUCHU ---
+                if (WykonajRuchWarcaby(planszaW, w1, k1, w2, k2, obecny_warcaby))
+                {
+                    // LOGIKA PROMOCJI (DAMKA)
+                    // Jeśli biały ('b') dotarł do wiersza 0 -> staje się 'B'
+                    if (obecny_warcaby == 'b' && w2 == 0) planszaW[w2][k2] = 'B';
+                    // Jeśli czarny ('c') dotarł do wiersza 7 -> staje się 'C'
+                    if (obecny_warcaby == 'c' && w2 == 7) planszaW[w2][k2] = 'C';
+
+                    // Aktualizacja i zmiana tury
+                    AktualizujSVG_Warcaby(planszaW);
+                    obecny_warcaby = (obecny_warcaby == 'b' ? 'c' : 'b');
+                }
+                else
+                {
+                    cout << "Blad ruchu! Wcisnij ENTER..." << endl;
+                    cin.ignore(); cin.get();
+                }
+            }
+            break; 
+        }
+            case 6: 
+            cout << "=== POMOC ===" << endl;
+            cout << "1. Kolko i Krzyzyk: Wybieraj pola numeryczne 1-9." << endl;
+            cout << "2. Warcaby: Ruchy podajesz jako 4 liczby: wiersz_z kolumna_z wiersz_do kolumna_do." << endl;
+            cout << "   Przyklad: '5 0 4 1' przesuwa pionek z pola (5,0) na (4,1)." << endl;
+            cout << "   Biale (b) startuja na dole (wiersze 5-7), Czarne (c) na gorze." << endl;
+            cout << "   Aby zapisac gre wpisz: 8 8 8 8. Aby wczytac: 7 7 7 7." << endl;
+            cout << "Wcisnij ENTER aby wrocic." << endl;
+            cin.get();
+            break;
             case 7: cout << "Zamykanie programu..." << endl; break;
             default: cout << "Nieznana opcja!" << endl; break;
         cout << endl;
